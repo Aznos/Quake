768af314ad45f06e93b589c886bd8a5db7652929
Aznos <91508926+Aznos@users.noreply.github.com>
2025-05-23 19:22:32 -0400

    GDT testing

--- src/gdt/gdt.asm
+++ src/gdt/gdt.asm
@@ -2,31 +2,13 @@
 
 global i686_GDT_Load
 i686_GDT_Load:
-  ;save frame
-  push rbp
-  mov rbp, rsp
+  lgdt [rdi]
 
-  ;load gdt
-  mov rax, [rbp + 16]
-  lgdt [rax]
-
-  ;reload code segs
-  mov ax, [rbp + 24]
-  push rax
-  lea rax, [rel .reload_cs]
-  push rax
-  retfq
-
-.reload_cs:
-  ;load data segs
-  mov ax, [rbp + 32]
+  mov ax, dx
   mov ds, ax
   mov es, ax
   mov fs, ax
   mov gs, ax
   mov ss, ax
 
-  ;restore frame
-  mov rsp, rbp
-  pop rbp
   ret
