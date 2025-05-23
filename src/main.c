d66b1a74501b655ee382593e8e597f836bb29096
Aznos <91508926+Aznos@users.noreply.github.com>
2025-05-22 21:32:59 -0400

    Project setup

---
+++ src/main.c
@@ -0,0 +1,39 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <limine.h>
+
+__attribute__((used, section(".limine_requests"))) static volatile LIMINE_BASE_REVISION(3);
+
+__attribute__((used, section(".limine_requests"))) static volatile struct limine_framebuffer_request fb_req = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST,
+    .revision = 0};
+
+__attribute__((used, section(".limine_requests_start"))) static volatile LIMINE_REQUESTS_START_MARKER;
+
+__attribute__((used, section(".limine_requests_end"))) static volatile LIMINE_REQUESTS_END_MARKER;
+
+static void hcf(void)
+{
+    for (;;)
+    {
+        asm("hlt");
+    }
+}
+
+void kmain(void)
+{
+    if (LIMINE_BASE_REVISION_SUPPORTED == false)
+        hcf();
+    if (fb_req.response == NULL || fb_req.response->framebuffer_count < 1)
+        hcf();
+
+    struct limine_framebuffer *fb = fb_req.response->framebuffers[0];
+    for (size_t i = 0; i < 100; i++)
+    {
+        volatile uint32_t *ptr = fb->address;
+        ptr[i * (fb->pitch / 4) + i] = 0xFF00FF00;
+    }
+
+    hcf();
+}
