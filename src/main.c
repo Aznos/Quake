da7970c202324c0dfbbbb6d2d80333a104f441b2
Aznos <91508926+Aznos@users.noreply.github.com>
2025-05-23 19:22:47 -0400

    GDT done! removed testing

--- src/main.c
+++ src/main.c
@@ -31,25 +31,7 @@ void kmain(void)
     terminal_init(fb);
 
     HAL_init();
+
     put_str(fb, 0, 0, "Hello world!", 0xFFFFFFFF, 0xFF000000);
-
-    asm volatile("sgdt %0" : "=m"(gdtr_readback));
-
-    if (gdtr_readback.limit != g_GDTDescriptor.limit)
-        put_str(fb, 0, 2, "GDT limit mismatch!", 0xFFFF0000, 0);
-    else if (gdtr_readback.base != (uint64_t)g_GDTDescriptor.ptr)
-        put_str(fb, 0, 3, "GDT base mismatch!", 0xFFFF0000, 0);
-    else
-        put_str(fb, 0, 2, "GDT looks OK", 0xFF00FF00, 0);
-
-    uint16_t ds, ss;
-    asm volatile("mov %%ds, %0" : "=r"(ds));
-    asm volatile("mov %%ss, %0" : "=r"(ss));
-
-    if (ds == i686_GDT_DATA_SEGMENT && ss == i686_GDT_DATA_SEGMENT)
-        put_str(fb, 0, 4, "DS/SS selectors OK", 0xFF00FF00, 0);
-    else
-        put_str(fb, 0, 4, "Bad DS/SS selectors!", 0xFFFF0000, 0);
-
     hcf();
 }
